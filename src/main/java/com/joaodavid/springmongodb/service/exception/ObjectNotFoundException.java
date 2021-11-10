2a49fed60e44198d30b0add3044ad621638b28ba
Jdavid77 <jdavid72009@hotmail.com>
2021-11-10 11:24:21 +0000

    Obtendo usuário por ID

---
+++ src/main/java/com/joaodavid/springmongodb/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.joaodavid.springmongodb.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
