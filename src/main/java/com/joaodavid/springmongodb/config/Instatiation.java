0eb0407f9e212d8f6ba354075fcbe7838ccdb4f9
Jdavid77 <jdavid72009@hotmail.com>
2021-11-11 12:54:32 +0000

    Inserindo comments

--- src/main/java/com/joaodavid/springmongodb/config/Instatiation.java
+++ src/main/java/com/joaodavid/springmongodb/config/Instatiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.joaodavid.springmongodb.domain.Post;
 import com.joaodavid.springmongodb.domain.User;
 import com.joaodavid.springmongodb.dto.AuthorDTO;
+import com.joaodavid.springmongodb.dto.CommentDTO;
 import com.joaodavid.springmongodb.repository.PostRepository;
 import com.joaodavid.springmongodb.repository.UserRepository;
 
@@ -41,6 +42,13 @@ public class Instatiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu Viagem","Vou viajar para São Paulo abraços",new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!!",new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!",sdf.parse("21/03/2018"),new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!",sdf.parse("22/03/2018"),new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!",sdf.parse("23/03/2018"),new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c3));
+		post2.getComments().add(c2);
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
